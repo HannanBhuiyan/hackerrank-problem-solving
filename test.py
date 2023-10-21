2ba29e00a0c408cf2d21ec3ae1c03c2113035440
HannanBhuiyan <hannan.diuh@gmail.com>
2023-10-21 09:14:05 -0700

    upload

---
+++ test.py
@@ -0,0 +1,16 @@
+
+
+fruits = ("apple", "mango", "lichi", "jambora", "mango")
+
+
+# print(fruits.count("mango"))
+# print(len(fruits))
+# print(fruits.index("mango", 2, 3))
+
+# x = list(fruits)
+# y = list(fruitsTwo)
+# z = x + y
+#
+# fruits_three = tuple(z)
+# print(fruits_three)
+
